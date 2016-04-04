a1afe78293cbf24066ddd2cc5ca5ecdb040268b0
sophieXF <xuefei.ee@gmail.com>
2016-04-03 23:39:29 -0700

    assignment 2 initialized

---
+++ Assignment2/RandomizedQueue.java
@@ -0,0 +1,20 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    // construct an empty randomized queue
+   public RandomizedQueue() {}
+   // is the queue empty?
+   public boolean isEmpty() {}
+   // return the number of items on the queue
+   public int size() {}
+   // add the item
+   public void enqueue(Item item) {}
+   // remove and return a random item
+   public Item dequeue() {}
+   // return (but do not remove) a random item
+   public Item sample() {}
+   // return an independent iterator over items in random order
+   public Iterator<Item> iterator() {} 
+   // unit testing
+   public static void main(String[] args) {}
+}
